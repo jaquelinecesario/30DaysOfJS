b4d0f91668a1c1f57652b74577289f16de006bad
jaquelinecesario <138641462+jaquelinecesario@users.noreply.github.com>
2023-10-03 16:43:34 -0300

    Update exercise-level-3.js

--- 02_Day_Data_types/exercise-level-3.js
+++ 02_Day_Data_types/exercise-level-3.js
@@ -8,7 +8,7 @@ console.log(string.match(/because/gi).length);
 // g-means to search in the whole text, i - case insensitive
 
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-console.log(sentence.replace(/%/g, '').replace(/\$/, '').replace(/@/g, '').replace(/&/g, '').replace(/#/g, '').replace(/;/g, ''));
+console.log(sentence.replace(/%/g, '').replace(/\$/g, '').replace(/@/g, '').replace(/&/g, '').replace(/#/g, '').replace(/;/g, ''));
 
 let annualIncome = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month'
 let numbers = annualIncome.match(/\d+/g)
